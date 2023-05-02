358300792c6194c3fa1c5f13760ca3006d3ed52e
Vasiliy <prohozhevva@gmail.com>
2023-05-02 21:58:11 +0300

    анимации на кнопках и комментарии

--- script_click.js
+++ script_click.js
@@ -5,17 +5,18 @@ let v = 0;
 let c = 1;
 let i = 0;
 butt.onclick = function() {
-    i = i + c;
+    i = i + c;  
     v = v + c;
     text.textContent = i;
     textall.textContent = 'Всего: ' + (v);
+    //Основные клики по печеньке
 }
 
 buttt.onclick = function() {
     let val = document.getElementById('elem1').value;
     f = val * 3;
     document.getElementById('str').innerHTML = "Стоимость: " + f;
-
+    //Определение стоимости
 }
 
 
@@ -28,60 +29,15 @@ go.onclick = function() {
         text.textContent = i;
         c = c + parseInt(buy2);
     }
+    //Покупка
 };
 
 png1.onclick = function() {
-    document.body.style.backgroundImage = 'url(1.png)';    
+    document.body.style.backgroundImage = 'url(1.png)';    //Смена фона на первый
 }
 png2.onclick = function() {
-    document.body.style.backgroundImage = 'url(2.png)';    
+    document.body.style.backgroundImage = 'url(2.png)';    //Смена фона на второй
 }
 png3.onclick = function() {
-    document.body.style.backgroundImage = 'url(3.png)';    
+    document.body.style.backgroundImage = 'url(3.png)';    //Смена фона на третий
 }
-
-/*
-document.addEventListener('click', function(e) {
-    if(e.target.className == "png1") {
-        png.src = '2.png';
-    }
-})
-*/
-/*
-document.addEventListener("click", function(e) {
-    if (e.target.className == "button") {
-        if (i > 50) {
-            i = i - 50;
-            c++
-            text.textContent = (i);
-        }
-    }
-});
-document.addEventListener("click", function(e) {
-    if (e.target.className == "button100") {
-        if (i > 100) {
-            i = i - 100;
-            c = c + 10
-            text.textContent = (i);
-        }
-    }
-});
-document.addEventListener("click", function(e) {
-    if (e.target.className == "button1000") {
-        if (i > 1000) {
-            i = i - 1000;
-            c = c + 100
-            text.textContent = (i);
-        }
-    }
-});
-document.addEventListener("click", function(e) {
-    if (e.target.className == "button10000") {
-        if (i > 10000) {
-            i = i - 10000;
-            c = c + 1000
-            text.textContent = (i);
-        }
-    }
-})
-*/
